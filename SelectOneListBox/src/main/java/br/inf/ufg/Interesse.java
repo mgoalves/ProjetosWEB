89b30f3ef0f2d8897566004c00e047c84891fd52
Marcello <marcello_doalves@hotmail.com>
2017-07-03 10:26:55 -0300

    SelectOneListBox

---
+++ SelectOneListBox/src/main/java/br/inf/ufg/Interesse.java
@@ -0,0 +1,43 @@
+package br.inf.ufg;
+
+import java.io.Serializable;
+
+public class Interesse implements Serializable {
+
+	private static final long serialVersionUID = 1L;
+
+	private String descricao;
+	private String nomeIcone;
+	
+	
+	Interesse () {
+		
+	}
+
+	Interesse (String descricao, String nomeIcone){
+		
+		this.descricao = descricao;
+		this.nomeIcone = nomeIcone;
+		
+		
+	}
+	
+	
+	//G and S
+	public String getDescricao() {
+		return descricao;
+	}
+
+	public void setDescricao(String descricao) {
+		this.descricao = descricao;
+	}
+
+	public String getNomeIcone() {
+		return nomeIcone;
+	}
+
+	public void setNomeIcone(String nomeIcone) {
+		this.nomeIcone = nomeIcone;
+	}
+
+}
