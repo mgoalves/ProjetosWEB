c377c1b65836cd6bd271ca93f12a39b1994ef57b
Marcello <marcello_doalves@hotmail.com>
2017-07-15 00:01:45 -0300

    Pesquisar Produto

---
+++ PedidoVenda/src/main/java/br/inf/ufg/pedidovenda/controller/PesquisaProdutoBean.java
@@ -0,0 +1,36 @@
+package br.inf.ufg.pedidovenda.controller;
+
+import java.io.Serializable;
+import java.util.ArrayList;
+import java.util.Formatter;
+import java.util.List;
+
+import javax.faces.bean.ManagedBean;
+import javax.faces.bean.RequestScoped;
+import javax.faces.bean.ViewScoped;
+
+@ManagedBean
+@RequestScoped
+@ViewScoped
+public class PesquisaProdutoBean implements Serializable {
+
+	private static final long serialVersionUID = 1L;
+	
+	private List<Integer> produtosFiltrados;
+
+	public PesquisaProdutoBean() {
+
+		produtosFiltrados = new ArrayList<>();
+
+		for (int i = 0; i < 50; i++) {
+
+			produtosFiltrados.add(i);
+			
+		}
+	}
+	
+	public List<Integer> getProdutoFiltrados() {
+
+		return produtosFiltrados;
+	}
+}
