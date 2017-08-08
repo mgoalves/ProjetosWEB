b7e08558ea5168d5f0940f49b46132bfda7209f1
Marcello <marcello_doalves@hotmail.com>
2017-08-08 11:23:41 -0300

    Não Funcionando. Target Unreacheable

--- PedidoVenda/src/main/java/br/inf/ufg/pedidovenda/controller/CadastroPedidoBean.java
+++ PedidoVenda/src/main/java/br/inf/ufg/pedidovenda/controller/CadastroPedidoBean.java
@@ -1,31 +1,44 @@
 package br.inf.ufg.pedidovenda.controller;
 
+import java.io.Serializable;
 import java.util.ArrayList;
 import java.util.List;
 
 import javax.enterprise.context.RequestScoped;
+import javax.faces.bean.ViewScoped;
 import javax.inject.Named;
 
-import br.inf.ufg.pedidovenda.service.NegocioException;
+import br.inf.ufg.pedidovenda.model.pedido.EnderecoEntrega;
+import br.inf.ufg.pedidovenda.model.pedido.Pedido;
 
+@SuppressWarnings("deprecation")
 @Named
+@ViewScoped
 @RequestScoped
-public class CadastroPedidoBean {
+public class CadastroPedidoBean implements Serializable {
+
+	private static final long serialVersionUID = 1L;
+
+	private Pedido pedido;
 
 	private List<Integer> itens;
-	
+
 	public CadastroPedidoBean() {
-		
+		pedido = new Pedido();
+		pedido.setEnderecoEntrega(new EnderecoEntrega());
 		itens = new ArrayList<>();
 		itens.add(1);
 	}
-	
-	public List<Integer> getItens() {	
+
+	public void salvar() {
+	}
+
+	public List<Integer> getItens() {
 		return itens;
 	}
-	
-	public void salvar(){
-		throw new NegocioException("Não implementado.");
+
+	public Pedido getPedido() {
+		return pedido;
 	}
 
 }
