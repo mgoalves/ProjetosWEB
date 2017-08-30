4df56051899291370f9a845a0f1b5c5fa4648353
Unknown <marcello_doalves@hotmail.com>
2017-08-30 10:31:06 -0300

    criando grafico

---
+++ PedidoVenda/src/main/java/br/inf/ufg/pedidovenda/controller/GraficosPedidosCriadosBean.java
@@ -0,0 +1,49 @@
+package br.inf.ufg.pedidovenda.controller;
+
+import java.io.Serializable;
+
+import javax.enterprise.context.RequestScoped;
+import javax.inject.Named;
+
+import org.primefaces.model.chart.CartesianChartModel;
+import org.primefaces.model.chart.ChartSeries;
+
+@Named
+@RequestScoped
+public class GraficosPedidosCriadosBean implements Serializable {
+
+	private static final long serialVersionUID = 1L;
+	
+	private CartesianChartModel model;
+	
+	public void preRender() {
+		this.model = new CartesianChartModel();
+		
+		adicionarSerie("Todos os pedidos");
+		adicionarSerie("Meus pedidos");
+		
+	}
+
+	private void adicionarSerie(String rotulo) {
+		
+		ChartSeries series = new ChartSeries(rotulo);
+		
+		series.set("1", Math.random() * 1000);
+		series.set("2", Math.random() * 1000);
+		series.set("3", Math.random() * 1000);
+		series.set("4", Math.random() * 1000);
+		series.set("5", Math.random() * 1000);
+		
+		this.model.addSeries(series);
+		
+		
+	}
+
+	public CartesianChartModel getModel() {
+		return model;
+	}
+
+
+	
+	
+}
